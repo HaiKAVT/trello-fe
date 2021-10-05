827efe1873e22f6bda17067af4d3a66ee6dff682
zhuy-nguyenz <graio911@gmail.com>
2021-10-05 17:57:27 +0700

    'Login'

---
+++ src/app/service/authenticate.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import {Observable} from "rxjs";
+
+const API_URL = `${environment.api_url}`;
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticateService {
+  constructor(private httpClient: HttpClient) {
+  }
+
+  login(username: string, password: string): Observable<any> {
+    return this.httpClient.post(API_URL + 'login', {username, password}, httpOptions)
+  }
+
+  logout() {
+    window.sessionStorage.clear()
+  }
+}
