739942895e5145c506e42596bbe30fcd00b34757
zhuy-nguyenz <graio911@gmail.com>
2021-10-05 19:12:29 +0700

    trello home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AuthGuard} from "./helper/auth.guard";
-import {HomeComponent} from "./create/home/home.component";
 import {LoginComponent} from "./login/login.component";
 
 const routes: Routes = [
   {
     path:'',
     canActivate:[AuthGuard],
-    component:HomeComponent
+    loadChildren: ()=> import('./trello/trello.module').then(module => module.TrelloModule)
   },
   {
     path:'login',
---
+++ src/app/trello/trello.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TrelloRoutingModule } from './trello-routing.module';
+import { HomeComponent } from './home/home.component';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    TrelloRoutingModule
+  ]
+})
+export class TrelloModule { }
