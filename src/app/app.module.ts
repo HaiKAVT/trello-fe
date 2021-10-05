64faf6d491134001ef87d7dbbdf022748d410e22
HaiKAVT <haivtka@gmail.com>
2021-10-05 17:37:01 +0700

    update model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { RecoverPasswordComponent } from './recover-password/recover-password.component';
+import { FooterComponent } from './share/footer/footer.component';
+import { ModalComponent } from './share/modal/modal.component';
+import { NavbarBoardHeaderComponent } from './share/navbar-board-header/navbar-board-header.component';
+import { NavbarComponent } from './share/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SignUpComponent,
-    RecoverPasswordComponent
+    RecoverPasswordComponent,
+    FooterComponent,
+    ModalComponent,
+    NavbarBoardHeaderComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
