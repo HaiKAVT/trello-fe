d777e423f39c80c07e281f072b694475575fb88b
zhuy-nguyenz <graio911@gmail.com>
2021-10-11 19:51:16 +0700

    update homepage

--- src/app/share/side-bar/side-bar/side-bar.component.ts
+++ src/app/share/side-bar/side-bar/side-bar.component.ts
@@ -1,6 +1,9 @@
 import {Component, Input, OnInit} from '@angular/core';
 
 import {AuthenticateService} from "../../../service/authenticate.service";
+import {Workspace} from "../../../model/workspace";
+import {UserToken} from "../../../model/user-token";
+import {WorkspaceService} from "../../../service/workspace/workspace.service";
 
 @Component({
   selector: 'app-side-bar',
@@ -8,14 +11,34 @@ import {AuthenticateService} from "../../../service/authenticate.service";
   styleUrls: ['./side-bar.component.css']
 })
 export class SideBarComponent implements OnInit {
-  constructor(private authenticateService: AuthenticateService) {
+  workspaces: Workspace[] = [];
+  loggedInUser!: UserToken;
+
+  constructor(private authenticateService: AuthenticateService,
+              private workspaceService: WorkspaceService) {
   }
 
+
   ngOnInit(): void {
+    this.loggedInUser = this.authenticateService.getCurrentUserValue()
+    this.getAllWorkspace();
+  }
+
+  getAllWorkspace() {
+    this.workspaceService.findAllByOwnerId(this.loggedInUser.id).subscribe(data => {
+      this.workspaces = data;
+    })
   }
 
   showCreateWorkspaceModal() {
     document.getElementById('create-workspace')!.classList.add('is-active');
   }
-
+  showWorkspaceButton(id:any){
+    let toChange = document.getElementById(`workspace-${id}`);
+    if(toChange!.classList.contains('is-hidden')){
+      toChange!.classList.remove('is-hidden')
+    } else {
+      toChange!.classList.add('is-hidden')
+    }
+  }
 }
---
+++ src/app/trello/workspace/workspacehome/workspace-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-workspacehome',
+  templateUrl: './workspace-home.component.html',
+  styleUrls: ['./workspace-home.component.css']
+})
+export class WorkspaceHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
