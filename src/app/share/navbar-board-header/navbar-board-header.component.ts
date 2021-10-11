9ef1fd5a0697e1e351f8109d38b5915c7802aa31
Nguyễn Hoài Nam <83760339+foenixice1@users.noreply.github.com>
2021-10-11 20:07:27 +0700

    FE Moi

--- src/app/share/navbar-board-header/navbar-board-header.component.ts
+++ src/app/share/navbar-board-header/navbar-board-header.component.ts
@@ -4,6 +4,8 @@ import {UserToken} from "../../model/user-token";
 import {AuthenticateService} from "../../service/authenticate.service";
 import {Router} from "@angular/router";
 import {BoardService} from "../../service/board/board.service";
+import {DetailedMember} from "../../model/detailed-member";
+import {Tag} from "../../model/tag";
 
 @Component({
   selector: 'app-navbar-board-header',
@@ -13,6 +15,9 @@ import {BoardService} from "../../service/board/board.service";
 export class NavbarBoardHeaderComponent implements OnInit {
   @Input() currentBoard: Board = {columns: [], owner: {}, title: "", tags: []};
   @Input() canEdit: boolean = false;
+  @Input() members: DetailedMember[] = [];
+  @Input() tags: Tag[] = [];
+  selectedMember: DetailedMember = {boardId: -1, canEdit: false, id: -1, userId: -1, username: ""};
   currentUser: UserToken = this.authenticationService.getCurrentUserValue()
   constructor(private authenticationService:AuthenticateService,
               private router:Router,
@@ -26,6 +31,35 @@ export class NavbarBoardHeaderComponent implements OnInit {
       this.boardService.updateBoard(this.currentBoard.id,this.currentBoard).subscribe(data=>{
         this.currentBoard = data
       })
+    }
+  }
+  showDetail(member: DetailedMember) {
+    this.selectedMember = member;
+    // @ts-ignore
+    document.getElementById('user-detail-modal').classList.add('is-active');
+  }
+
+  showUserPreview(member: DetailedMember) {
+    let elementId = 'user-preview-text-' + member.userId;
+    let element = document.getElementById(elementId);
+    // @ts-ignore
+    element.innerHTML = '@' + member.username;
+    // @ts-ignore
+    element.classList.remove('is-hidden');
+  }
+  closeUserPreviews() {
+    let elements = document.getElementsByClassName('user-preview-text');
+    // @ts-ignore
+    for (let element of elements) {
+      element.classList.add('is-hidden');
+    }
+  }
+
+  showAllMembers() {
+    let members = document.getElementsByClassName('user-preview');
+    // @ts-ignore
+    for (let member of members) {
+      member.classList.remove('is-hidden');
     }
   }
 }
