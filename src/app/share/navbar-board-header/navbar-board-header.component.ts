37e3066daae2a28d1911cee6eeecef77ccb44426
Nguyễn Hoài Nam <83760339+foenixice1@users.noreply.github.com>
2021-10-12 21:42:24 +0700

    update

--- src/app/share/navbar-board-header/navbar-board-header.component.ts
+++ src/app/share/navbar-board-header/navbar-board-header.component.ts
@@ -78,7 +78,7 @@ export class NavbarBoardHeaderComponent implements OnInit {
   createNoticeInBoard(activityText: string) {
     let activity: ActivityLog = {
       title: "Bảng : " + this.currentBoard.title,
-      content: this.currentUser.username + " " + activityText + " trong " + this.currentBoard.title + " " + this.notificationService.getTime(),
+      content: this.currentUser.username + " " + activityText + " trong " + this.currentBoard.title + " " + "Vào : " + this.notificationService.getTime(),
       url: "/trello/board/" + this.currentBoard.id,
       status: false,
       board: this.currentBoard
